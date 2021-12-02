d9f505fb0e5edb8a431452725d81bece4c00772d
36kyo <kyotar@gmail.com>
2021-12-02 20:06:34 +0900

    update thick line color settings

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@ void drawCircleWeight(M5GFX *disp, int32_t x, int32_t y, int32_t r, int32_t weig
   disp->fillArc(x, y, r-weight/2.0, r+weight/2.0, 0, 360);
 }
 
-void drawLineWeight(M5GFX *disp, int32_t x0, int32_t y0, int32_t x1, int32_t y1, uint8_t weight_px, uint8_t color565){
-  uint8_t img[1] = {color565};
+void drawLineWeight(M5GFX *disp, int32_t x0, int32_t y0, int32_t x1, int32_t y1, uint8_t weight_px, uint16_t color565){
+  uint8_t img[1] = {disp->color16to8(color565)};
   
   float angle = degrees( atan2f(y1 - y0, x1 - x0) );
   float len = sqrtf(powf(x1 - x0, 2) + powf(y1 - y0, 2));
@@ -23,6 +23,9 @@ void drawLineWeight(M5GFX *disp, int32_t x0, int32_t y0, int32_t x1, int32_t y1,
     1, 1,       // image w, h 
     img );
 }
+// void drawLineWeight(M5GFX *disp, int32_t x0, int32_t y0, int32_t x1, int32_t y1, uint8_t weight_px){
+//   drawLineWeight(disp, x0, y0, x1, y1, weight_px, disp->getColor());
+// }
 
 void setup() {
   // put your setup code here, to run once:
@@ -33,7 +36,7 @@ void setup() {
   display.setColor(TFT_WHITE);
   drawCircleWeight(&display, display.width()/2.0, display.height()/2.0, 50, 10);
 
-  drawLineWeight(&display, 0,0, 200,200, 10, 0xFF);
+  drawLineWeight(&display, 0,0, 200,200, 10, TFT_WHITE);
 }
 
 void loop() {
